61e729798e52bf9048ac9b458f6dc94ed5d6ce23
jiayin <501023696@qq.com>
2018-11-09 17:04:59 +0800

    1

---
+++ src/main/java/com/blw/yjydemo/YjyDemoApplication.java
@@ -0,0 +1,12 @@
+package com.blw.yjydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YjyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YjyDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/blw/yjydemo/YjyDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.blw.yjydemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YjyDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
